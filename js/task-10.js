cb9a9f67f305389debb3e03d7f3bb804729f390f
Joanna K <joannakleinrm@gmail.com>
2023-02-20 22:48:01 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const amount = document.querySelector("#controls input");
+
+
+
+
+
+function createBoxes(amount) {
+  // Utwórz funkcję createBoxes(amount), która bierze jeden parametr - liczbę.Funkcja tworzy tyle < div >, ile ukazano w amount i dodaje je do div#boxes.
+
+}
+
+
+function removeBoxes() {
+  // destroyBoxes()-- > usuwa zawartość div#boxes
+  
+}
+
+
+// Wymiary pierwszego < div > - 30px na 30px.
+// Każdy następny element powinien być szerszy i wyższy od poprzedniego o 10px.
+// Wszystkie elementy powinny mieć losowy kolor tła w formacie NEX.Użyj gotowej funkcji getRandomHexColor aby otrzymać kolor.
+  
